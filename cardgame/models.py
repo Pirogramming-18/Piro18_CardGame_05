e7f0ee17405e9c4201ef22b8cbe713810690e48d
oh-bom <spdlqj888888@gmail.com>
2023-01-20 00:59:24 +0900

    장고로그인 user model적용전

--- cardgame/models.py
+++ cardgame/models.py
@@ -1,4 +1,7 @@
+from django.db import models
 from random import choice
+from django.contrib.auth.models import AbstractUser
+
 
 class Card(models.Model):
 	card_num = models.IntegerField()
@@ -6,17 +9,28 @@ class Card(models.Model):
 	def __str__(self):
 		return str(self.card_num)
 
+class User(AbstractUser):
+    
+	first_name=None
+	last_name=None #필드 삭제
+    
+	email=models.EmailField(null=True)
+	name = models.CharField(max_length=64)
+	score = models.IntegerField()
+	card = models.ForeignKey(Card, on_delete=models.CASCADE, related_name="User_game")
+
+
 class Attack(models.Model):
 	attack_user = models.ForeignKey(User, on_delete=models.CASCADE)
 	num = models.ForeignKey(Card, on_delete=models.CASCADE)
-  attacked_user = models.ForeignKey(User, on_delete=models.CASCADE)
+	attacked_user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Defense(models.Model):
 	defense_user = models.ForeignKey(Attack, on_delete=models.CASCADE)
 	num = models.ForeignKey(Card, on_delete=models.CASCADE, null=True, blank=True)
 
 MODE_CHOICES = {('큰 수', '큰 수'), ('작은 수', '작은 수')}
-STATUS_CHOICES = {'진행 중', '진행 중'), ('끝', '끝')}
+STATUS_CHOICES = {('진행 중', '진행 중'), ('끝', '끝')}
 class CardGame(models.Model):
 	mode = models.CharField(max_length=30, choices = MODE_CHOICES) # 큰 수가 이기는 게임 혹은 작은 수가 이기는 게임
 	status = models.CharField(max_length=30, choices = STATUS_CHOICES) # 게임 상태값
@@ -24,23 +38,4 @@ class CardGame(models.Model):
 	defense = models.OnetoOneField(Defense, on_delete=models.CASCADE)
 	win_user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
-class User(models.Model):
-	name = models.CharField(max_length=64)
-	score = models.IntegerField()
-	card = models.ForeignKey(Card, on_delete=models.CASCADE, related_name="User_game")
 
-#소셜 로그인
-NAVER_CALLBACK_URI = BASE_URL + 'naver/callback/'
-GOOGLE_CALLBACK_URI = BASE_URL + 'google/callback/'
-
-class NaverLogin(SocialLoginView):
-    adapter_class = NaverOAuth2Adapter
-    callback_url = NAVER_CALLBACK_URI
-    client_class = OAuth2Client
-    serializer_class = SocialLoginSerializer
-
-class GoogleLogin(SocialLoginView):
-    adapter_class = GoogleOAuth2Adapter
-    callback_url = GOOGLE_CALLBACK_URI
-    client_class = OAuth2Client
-    serializer_class = SocialLoginSerializer
---
+++ cardgame/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns =[
+    # path('rest-auth/naver/', views.NaverLogin.as_view(), name='naver'),
+    # path('rest-auth/google/', views.GoogleLogin.as_view(), name='google'),
+ 
+ ]
