232088c31b0dae3b72ee6c74d02d8e3e1dcbf125
oh-bom <spdlqj888888@gmail.com>
2023-01-21 01:02:54 +0900

    hw정리

--- cardgame/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns =[
-    # path('rest-auth/naver/', views.NaverLogin.as_view(), name='naver'),
-    # path('rest-auth/google/', views.GoogleLogin.as_view(), name='google'),
- 
- ]
