17e09b67803dd0685cb490234f5c297ccf2a21b2
damdami <okcall0302@gmail.com>
2023-01-20 23:50:54 +0900

    공백 제거

--- cardgame/views.py
+++ cardgame/views.py
@@ -9,7 +9,6 @@ def game_list(request, *args, **kwargs):
     cardgames = CardGame.objects.all()
 
     return render(request, "game_list.html", {"cardgames":cardgames})
-    
 
 def user_ranking(request:HttpRequest, *args, **kwargs):
     users = User.objects.all().order_by("-score")
